cfb49251a4880b45f0e8d6d70f79ee5332b451e9
yasmine hassan <yasminehassan2621@gmail.com>
2021-07-13 16:21:07 +0200

    final project :)

--- src/threads/math_manipulation.h
+++ src/threads/math_manipulation.h
@@ -1,8 +1,6 @@
 #ifndef THREADS_REAL_H
 #define THREADS_REAL_H
-
 #define F 16384
-
 #define INT_TO_REAL(n) (n) * (F)
 #define REAL_TO_INT(x) (x) / (F)
 #define REAL_TO_INT_ROUND(x) ((x) >= 0 ? ((x) + (F) / 2) / (F) : ((x) - (F) / 2) / (F))
@@ -14,5 +12,4 @@
 #define REAL_DIV(x , y) ((int64_t)(x)) * (F) / (y)
 #define REAL_MULT_INT(x , n) (x) * (n)
 #define REAL_DIV_INT(x , n) (x) / (n)
-
 #endif
